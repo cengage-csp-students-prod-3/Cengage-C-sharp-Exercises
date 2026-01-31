39b703f64c80b14669628d9f8c95611263bf81bb
evapeck <evalpeck@gmail.com>
2026-01-31 23:37:33 +0000

    Testing Sat Jan 31 2026 23:37:33 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex01/student/PersonalInfo.cs
+++ chapter1/ex01/student/PersonalInfo.cs
@@ -6,6 +6,9 @@ class PersonalInfo
 	static void Main()
 	{
 		// Write your code here
-		Console.WriteLine("Harry");
+		Console.WriteLine("Lucinda Potter");
+		Console.WriteLine("6/24/1992");
+		Console.WriteLine("work 000-555-0101");
+		Console.WriteLine("cell 000-555-0189");
 	}
 }
