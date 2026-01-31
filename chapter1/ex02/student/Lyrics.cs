c22e77e28acb1c86bcfb3ee472d27c1f0d99e795
evapeck <evalpeck@gmail.com>
2026-01-31 23:47:40 +0000

    Testing Sat Jan 31 2026 23:47:40 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex02/student/Lyrics.cs
+++ chapter1/ex02/student/Lyrics.cs
@@ -6,5 +6,10 @@ class Lyrics
 	static void Main()
 	{
 		// Write your code here
+		System.Console.WriteLine("Oh no, I did it again");
+		System.Console.WriteLine("I let my mind leak out into all my friends");
+		System.Console.WriteLine("And now I'm lonely");
+		System.Console.WriteLine("Sitting and sink in the blue");
+
 	}
 }
