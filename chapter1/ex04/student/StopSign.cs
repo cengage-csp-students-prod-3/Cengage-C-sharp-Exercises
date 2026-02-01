355dffa29f8f5ca81b3dd10c62d6c8a8eaee208b
evapeck <evalpeck@gmail.com>
2026-02-01 00:47:18 +0000

    Testing Sun Feb 01 2026 00:47:18 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04/student/StopSign.cs
+++ chapter1/ex04/student/StopSign.cs
@@ -6,5 +6,13 @@ class StopSign
 	static void Main()
 	{
 		// Write your code here
+		WriteLine("   XXXXXX   ");
+		WriteLine(" X        X ");
+		WriteLine("X   STOP   X");
+		WriteLine(" X        X ");
+		WriteLine("   XXXXXX   ");
+		WriteLine("      X     ");
+		WriteLine("      X     ");
+		WriteLine("      X     ");
 	}
 }
