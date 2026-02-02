eb91cc366e38a2c3ee717ecc164e6aef7cf81a6e
evapeck <evalpeck@gmail.com>
2026-02-02 00:05:37 +0000

    Testing Mon Feb 02 2026 00:05:37 GMT+0000 (Coordinated Universal Time)

--- chapter1/de03/student/DebugOne3.cs
+++ chapter1/de03/student/DebugOne3.cs
@@ -1,13 +1,13 @@
 using static System.Console;
-classs DebugOne3
+class DebugOne3
 {
    static void Main()
    {
       WriteLine("This program lists the number 1 to 5 vertically");
-      Write("1");
-      Write("2");
-      Write("3");
-      Write("4");
-      Write("5");
+      WriteLine("1");
+      WriteLine("2");
+      WriteLine("3");
+      WriteLine("4");
+      WriteLine("5");
    }
 }
