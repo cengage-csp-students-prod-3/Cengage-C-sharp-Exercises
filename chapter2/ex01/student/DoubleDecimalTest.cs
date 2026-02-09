49cde7ef04fa01ca76e34f956dda830f231e948e
evapeck <evalpeck@gmail.com>
2026-02-09 04:31:51 +0000

    Testing Mon Feb 09 2026 04:31:51 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex01/student/DoubleDecimalTest.cs
+++ chapter2/ex01/student/DoubleDecimalTest.cs
@@ -6,5 +6,9 @@ class DoubleDecimalTest
 	static void Main()
 	{
 		// Write your code here
+		double doubleNumber = 1e54;
+		Write("Double: " + doubleNumber);
+		decimal decimalNumber = 1e54;
+		Write("Decimal: " + decimalNumber);
 	}
 }
