0b3cc58596b1bda5f6b889862922fecd033b2f4e
evapeck <evalpeck@gmail.com>
2026-02-09 04:39:15 +0000

    Testing Mon Feb 09 2026 04:39:15 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/InchesToCentimeters.cs
+++ chapter2/ex02/student/InchesToCentimeters.cs
@@ -6,5 +6,9 @@ class InchesToCentimeters
 	static void Main()
 	{
 		// Write your code here
+		const double CENT_IN_INCH = 2.54;
+		double inches = 3;
+		double centimeters = inches * CENT_IN_INCH;
+		Write("{0} inches is {1} centimeters.", inches, centimeters);
 	}
 }
