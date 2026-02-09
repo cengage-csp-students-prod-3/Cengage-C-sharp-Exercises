f66ea38d180978a7b0db5b0c118300fca98f3bd5
evapeck <evalpeck@gmail.com>
2026-02-09 04:49:18 +0000

    Testing Mon Feb 09 2026 04:49:18 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/InchesToCentimetersInteractive.cs
+++ chapter2/ex03/student/InchesToCentimetersInteractive.cs
@@ -6,5 +6,12 @@ class InchesToCentimeterslnteractive
 	static void Main()
 	{
 		// Write your code here
+		const double CENT_IN_INCH = 2.54;
+		double inches;
+		Write("Enter Inches: ");
+		inches = Convert.ToDouble(ReadLine());
+		double centimeters = inches * CENT_IN_INCH;
+		Write("{0} inches is {1} centimeters.", inches, centimeters);
+
 	}
 }
