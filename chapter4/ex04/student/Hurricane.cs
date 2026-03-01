7cfafa15b2041449ebe4feda0d4c6777ab55a148
evapeck <evalpeck@gmail.com>
2026-03-01 20:57:13 +0000

    Testing Sun Mar 01 2026 20:57:13 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/Hurricane.cs
+++ chapter4/ex04/student/Hurricane.cs
@@ -1,10 +1,35 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.ComponentModel;
 class Hurricane
 {
 	static void Main()
 	{
-		// Write your code here
+		string inputString;
+		int speed;
+		int cat;
+
+		WriteLine("Enter wind speed: ");
+		inputString = ReadLine();
+		speed = Convert.ToInt32(inputString);
+
+		if (speed >= 157)
+			cat = 5;
+		else if (speed >= 130)
+			cat = 4;
+		else if (speed >= 111)
+			cat = 3;
+		else if (speed >= 96)
+			cat = 2;
+		else if (speed >= 74)
+			cat = 1;
+		else
+			cat = 0;
+
+		if (cat > 0)
+			WriteLine("This is a category {0} hurricane.", cat);
+		else
+			WriteLine("This is not a hurricane.");
 	}
 }
