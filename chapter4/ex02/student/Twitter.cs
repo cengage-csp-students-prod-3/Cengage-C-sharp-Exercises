0eb581d04e70fae3e60b7e708ad7faeede2749da
evapeck <evalpeck@gmail.com>
2026-03-01 20:12:00 +0000

    Testing Sun Mar 01 2026 20:12:00 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/Twitter.cs
+++ chapter4/ex02/student/Twitter.cs
@@ -5,6 +5,16 @@ class Twitter
 {
 	static void Main()
 	{
-		// Write your code here
+		string inputString;
+		int messageLength;
+
+		WriteLine("Write your message: ");
+		inputString = ReadLine();
+		messageLength = inputString.length();
+
+		if (messageLength > 140)
+			WriteLine("The message is too long.");
+		else
+			WriteLine("The message is okay.");
 	}
 }
