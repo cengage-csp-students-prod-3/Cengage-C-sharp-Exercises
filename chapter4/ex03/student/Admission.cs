14e10e30e10eb09e147bc4a9d035f512eff48235
evapeck <evalpeck@gmail.com>
2026-03-01 20:32:29 +0000

    Testing Sun Mar 01 2026 20:32:29 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/Admission.cs
+++ chapter4/ex03/student/Admission.cs
@@ -5,6 +5,21 @@ class Admission
 {
 	static void Main()
 	{
-		// Write your code here
+		string inputString;
+		double GPA;
+		int admissionScore;
+
+		WriteLine("Enter a GPA: ");
+		inputString = ReadLine();
+		GPA = Convert.ToDouble(inputString);
+		WriteLine("Enter an admission score: ");
+		inputString = ReadLine();
+		admissionScore = Convert.ToInt32(inputString);
+
+		if ((GPA >= 3.0 && admissionScore >= 60) || (GPA < 3.0 && admissionScore >= 80))
+			WriteLine("Accept");
+		else
+			WriteLine("Reject");
+
 	}
 }
