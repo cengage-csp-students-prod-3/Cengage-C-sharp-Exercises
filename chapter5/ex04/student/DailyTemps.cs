4e3a7d3abbefc44d7878426247c4d9b4587b85dc
evapeck <evalpeck@gmail.com>
2026-03-16 03:05:51 +0000

    Testing Mon Mar 16 2026 03:05:51 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/DailyTemps.cs
+++ chapter5/ex04/student/DailyTemps.cs
@@ -5,6 +5,27 @@ class DailyTemps
 {
 	static void Main()
 	{
-		// Write your code here
+		int temp = 0;
+		int total = 0;
+		int count = 0;
+		double average = 0;
+		//WriteLine("Please enter a daily high temperature.");
+		//temp = Convert.ToInt32(ReadLine());
+
+		while (temp != 999)
+		{
+			WriteLine("Please enter a daily high temperature.");
+			temp = Convert.ToInt32(ReadLine());
+			if (temp <= 130 && temp >= -20)
+			{
+				total += temp;
+				count++;
+				average = total / count;
+			}
+			else
+			WriteLine("Valid temperatures range from -20 to 130. Please reenter temperatures.");
+		}
+		WriteLine("Number of temperatures entered: " + count);
+		WriteLine("Average temperature: " + average);
 	}
 }
