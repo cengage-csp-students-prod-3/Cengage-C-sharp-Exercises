3e06a0f6849e4279c33b15da2f02368a122e34e5
evapeck <evalpeck@gmail.com>
2026-03-16 02:46:51 +0000

    Testing Mon Mar 16 2026 02:46:50 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex03/student/EnterUppercaseLetters.cs
+++ chapter5/ex03/student/EnterUppercaseLetters.cs
@@ -5,6 +5,22 @@ class EnterUppercaseLetters
 {
 	static void Main()
 	{
-		// Write your code here
+	
+		WriteLine("Enter an uppercase letter");
+		String letter = ReadLine();
+
+		while (letter != "!") {
+			
+		if (letter == letter.ToUpper())
+			{
+				WriteLine("OK");
+			}
+		else
+			WriteLine("Sorry - that was not an uppercase letter.");
+		
+		WriteLine("Enter an uppercase letter or ! to quit");
+		letter = ReadLine();
+
+	}
 	}
 }
