e94f7620e0c79bffe98f2ba7250307a8ce5e05ed
evapeck <evalpeck@gmail.com>
2026-03-16 02:21:50 +0000

    Testing Mon Mar 16 2026 02:21:50 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/SumInts.cs
+++ chapter5/ex02/student/SumInts.cs
@@ -5,6 +5,16 @@ class SumInts
 {
 	static void Main()
 	{
-		// Write your code here
+		int sum = 0;
+		int number = 0;
+
+		while (number != 999)
+		{
+			WriteLine("Enter an integer: ");
+			number = Convert.ToInt32(ReadLine());
+			if (number != 999) {
+			sum += number; }
+		}
+		WriteLine("The sum of integers is " + sum);
 	}
 }
