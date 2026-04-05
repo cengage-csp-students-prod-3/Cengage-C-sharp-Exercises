8e9eebbc566b5df98faad9f8885bdcddb83f8489
evapeck <evalpeck@gmail.com>
2026-04-05 00:14:42 +0000

    Testing Sun Apr 05 2026 00:14:42 GMT+0000 (Coordinated Universal Time)

--- chapter5/de01/student/DebugFive1.cs
+++ chapter5/de01/student/DebugFive1.cs
@@ -6,16 +6,18 @@ class DebugFive1
 {
    static void Main()
    {
-      cont string PASS1 = "home";
-      cont string PASS2 = "lady";
-      cont string PASS3 = "mouse";
+      const string PASS1 = "home";
+      const string PASS2 = "lady";
+      const string PASS3 = "mouse";
       string password;
 
       Write("Please enter your password ");
       password = Console.ReadLine();
-      while(password != PASS1 || password != PASS2 || password != PASS3)
+      while(password != PASS1 && password != PASS2 && password != PASS3)
+      {
          WriteLine("Invalid password. Please enter again. ");
-      password = ReadLine();
+         password = ReadLine();
+      }
       WriteLine("Valid password");
    }
 }
