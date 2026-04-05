7f48fce7cce43bdb239b844f31454a902b2e5fce
evapeck <evalpeck@gmail.com>
2026-04-05 02:13:05 +0000

    Testing Sun Apr 05 2026 02:13:05 GMT+0000 (Coordinated Universal Time)

--- chapter6/de04/student/DebugSix04.cs
+++ chapter6/de04/student/DebugSix04.cs
@@ -8,31 +8,31 @@ class DebugSix04
    public static void Main()
    {
       const int QUIT = 999;
-      int[] numbers = new int[];
-      int x;
+      int[] numbers = new int[20];
+      int x = 0;
       int num;
       double average;
-      double total = 0;
+      double total = 0.0;
       string inString;
-      Console.Write("Please enter a number or " +
+      Write("Please enter a number or " +
          QUIT + " to quit...");
-      inString = ReadLine;
-      num = Convert.ToInt(inString);
-      while((x < numbers.Length) && num == QUIT)
+      inString = ReadLine();
+      num = Convert.ToInt32(inString);
+      while((x < numbers.Length) && num != QUIT)
       {
  	  numbers[x] = num;
           total += numbers[x];
           ++x;
           Write("Please enter a number or " +
              QUIT + " to quit...");
-          inString = readLine();
+          inString = ReadLine();
           num = Convert.ToInt32(inString);
       }
       WriteLine("The numbers are:");
-      for(int y = 0; y < x; ++x)
-          Console.Write("{0,6}", numbers[y]);
-      average = total / z;
+      for(int y = 0; y < x; ++y)
+          Write("{0,6}", numbers[y]);
+      average = total / x;
       WriteLine();
-      WriteLine("The average is {0}", avge;
+      WriteLine("The average is {0}", average);
   }
 }
