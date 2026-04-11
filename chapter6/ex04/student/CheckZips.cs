e141de4cb6ce6b97a47007849045c8ef6ecf625a
evapeck <evalpeck@gmail.com>
2026-04-11 18:22:27 +0000

    Testing Sat Apr 11 2026 18:22:27 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex04/student/CheckZips.cs
+++ chapter6/ex04/student/CheckZips.cs
@@ -1,12 +1,20 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.Linq;
 class CheckZips
 {
 	static void Main()
 	{
 		string[] zips = {"12789", "54012", "54481", "54982", "60007",
          "60103", "60187", "60188", "71244", "90210"};
-		// Write your code here
+		
+		Write("Enter a zip code: ");
+		string userzip = ReadLine();
+
+		if (zips.Contains(userzip))
+			WriteLine("Delivery to " + userzip + " OK");
+		else 
+			WriteLine("Sorry - no delivery to " + userzip);
 	}
 }
