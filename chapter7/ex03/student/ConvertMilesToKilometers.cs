92212cad603cdeabf6a11f366b1135c82e3d401f
evapeck <evalpeck@gmail.com>
2026-04-11 23:08:42 +0000

    Testing Sat Apr 11 2026 23:08:42 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex03/student/ConvertMilesToKilometers.cs
+++ chapter7/ex03/student/ConvertMilesToKilometers.cs
@@ -1,10 +1,22 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 class ConvertMilesToKilometers
 {
 	static void Main()
 	{
-		// Write your code here
+		WriteLine("Enter number of miles: ");
+		int miles = Convert.ToInt32(ReadLine());
+
+		double kms = ConvertToKilometers(miles);
+		WriteLine("{0} miles is {1} kilometers", miles, kms);
+	}
+
+	public static double ConvertToKilometers(int miles)
+	{
+		double kms = miles * 1.60934;
+		return kms;
 	}
 }
