a37077f8398e934d96040b8c436641658dc255fc
evapeck <evalpeck@gmail.com>
2026-04-11 18:44:50 +0000

    Testing Sat Apr 11 2026 18:44:50 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex05/student/DeliveryCharges.cs
+++ chapter6/ex05/student/DeliveryCharges.cs
@@ -5,6 +5,19 @@ class DeliveryCharges
 {
 	static void Main()
 	{
-		// Write your code here
+		string[] zips = {"12789", "54012", "54481", "54982", "60007", "60103", "60187", "60188", "71244", "90210"};
+		double[] prices = {2.40, 3.00, 3.50, 4.00, 4.50, 5.00, 5.25, 5.75, 6.10, 10.00};
+
+		WriteLine("Enter a zip code: ");
+		string userzip = ReadLine();
+
+		int index = Array.IndexOf(zips, userzip);
+
+
+		if (index != -1)
+			WriteLine("Delivery to {0} OK. Delivery charge is {1}", userzip, prices[index].ToString("C", CultureInfo.GetCultureInfo("en-US")));
+		else 
+			WriteLine("Sorry - no delivery to " + userzip);
+
 	}
 }
