37ca7d7714a5449dc6c284deedaad205a5e9ec8d
evapeck <evalpeck@gmail.com>
2026-04-11 21:27:24 +0000

    Testing Sat Apr 11 2026 21:27:24 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex08/student/ResortPrices.cs
+++ chapter6/ex08/student/ResortPrices.cs
@@ -1,10 +1,27 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.Numerics;
 class ResortPrices
 {
 	static void Main()
 	{
-		// Write your code here
+		int[] nightLimits = {2, 4, 7, int.MaxValue};
+		double[] rates = {200, 180, 160, 145};
+
+		WriteLine("Enter amount of nights: ");
+		int nights = Convert.ToInt32(ReadLine());
+
+		int index = 0;
+		double total;
+
+		while (nights > nightLimits[index])
+		{
+			index++;
+		}
+		
+		total = rates[index] * nights;
+
+		WriteLine("Price her night is {0} \nTotal for {1} night(s) is {2}", rates[index].ToString("C", CultureInfo.GetCultureInfo("en-US")), nights, total.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 	}
 }
