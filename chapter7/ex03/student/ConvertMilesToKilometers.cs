cc5cf3e38755f0ea40b889f16659ffc31f1a1758
evapeck <evalpeck@gmail.com>
2026-04-11 23:22:58 +0000

    Testing Sat Apr 11 2026 23:22:58 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex03/student/ConvertMilesToKilometers.cs
+++ chapter7/ex03/student/ConvertMilesToKilometers.cs
@@ -1,16 +1,13 @@
 using System;
 using static System.Console;
-using System.Globalization;
-using System.Reflection;
-using System.Reflection.Metadata.Ecma335;
 class ConvertMilesToKilometers
 {
 	static void Main()
 	{
 		WriteLine("Enter number of miles: ");
 		double miles = Convert.ToDouble(ReadLine());
-
 		double kms = ConvertToKilometers(miles);
+
 		WriteLine("{0} miles is {1} kilometers", miles, kms);
 	}
 
