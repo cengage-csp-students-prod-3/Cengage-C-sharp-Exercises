1e459124247124020f2df8869b18e22cfdbdea49
evapeck <evalpeck@gmail.com>
2026-04-11 22:58:27 +0000

    Testing Sat Apr 11 2026 22:58:27 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex02/student/PaintingEstimate.cs
+++ chapter7/ex02/student/PaintingEstimate.cs
@@ -1,10 +1,24 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.ComponentModel.Design.Serialization;
 class PaintingEstimate
 {
 	static void Main()
 	{
-		// Write your code here
+		WriteLine("Enter room length >> ");
+		int length = Convert.ToInt32(ReadLine());
+		WriteLine("Enter room width >> ");
+		int width = Convert.ToInt32(ReadLine());
+		double cost = PaintCost(length, width);
+
+		WriteLine("Cost of job for {0} X {1} foot room is {2}", length, width, cost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+	}
+
+	public static double PaintCost(int length, int width)
+	{
+		double area = (2 * length + 2 * width) * 9;
+		double cost = area * 6;
+		return cost;
 	}
 }
