f3423900b755be6c4836e3a31ea93b05ec615c6e
evapeck <evalpeck@gmail.com>
2026-04-11 18:01:43 +0000

    Testing Sat Apr 11 2026 18:01:43 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex03/student/TemperaturesComparison.cs
+++ chapter6/ex03/student/TemperaturesComparison.cs
@@ -5,6 +5,53 @@ class TemperaturesComparison
 {
 	static void Main()
 	{
-		// Write your code here
+		int[] temps = new int[5];
+		int total = 0;
+		
+		for (int i = 0; i < temps.Length; i++)
+		{
+			int temp;
+			
+			do
+			{
+				Write("Enter a temperature (-30 to 130): ");
+				temp = Convert.ToInt32(ReadLine());
+			
+				if (temp < -30 || temp > 130)
+				{
+					WriteLine("Invalid Temperature. Try again.");
+				}
+			}
+			while (temp < -30 || temp > 130);
+
+			temps[i] = temp;
+			total += temp;		
+		}
+
+		double average = (double)total / temps.Length;
+
+		bool ascending = true;
+		bool descending = true;
+
+		for (int i = 0; i < temps.Length - 1; i++)
+		{
+			if (temps[i] < temps[i + 1])
+				descending = false;
+			if (temps[i] > temps[i + 1])
+				ascending = false;
+		}
+
+		if (ascending)
+			Write("Getting warmer: ");
+		else if (descending)
+			Write("Getting cooler: ");
+		else
+			Write("It's a mixed bag: ");
+
+		for (int i = 0; i < temps.Length; i++)
+			Write(temps[i] + " ");
+		
+		WriteLine();
+		WriteLine("Average: " + average);
 	}
 }
