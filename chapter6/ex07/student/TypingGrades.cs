1d2b7d7ac0ec441ad79ce27d3ce62abfc5612377
evapeck <evalpeck@gmail.com>
2026-04-11 21:04:13 +0000

    Testing Sat Apr 11 2026 21:04:13 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex07/student/TypingGrades.cs
+++ chapter6/ex07/student/TypingGrades.cs
@@ -5,6 +5,19 @@ class TypingGrades
 {
 	static void Main()
 	{
-		// Write your code here
+		int[] wordLimit = {15, 30, 50, 75, int.MaxValue};
+		string[] grades = {"F", "D", "C", "B", "A"};
+
+		WriteLine("Enter number of words typed: ");
+		int inputWords = Convert.ToInt32(ReadLine());
+
+		int index = 0;
+
+		while (inputWords > wordLimit[index])
+		{
+			index++;
+		}
+
+		WriteLine("Typing {0} words per minute: Grade {1}.", inputWords, grades[index]);
 	}
 }
