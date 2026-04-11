e41f58499bd95d6196e22d88a9d5b63b98b528c2
evapeck <evalpeck@gmail.com>
2026-04-11 16:42:05 +0000

    Testing Sat Apr 11 2026 16:42:05 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex02/student/TestScoreList.cs
+++ chapter6/ex02/student/TestScoreList.cs
@@ -1,10 +1,33 @@
 using System;
 using static System.Console;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Numerics;
 class TestScoreList
 {
 	static void Main()
 	{
 		// Write your code here
+
+		int[] TestScoreList = new int[8];
+		int total = 0;
+
+
+		for (int i = 0; i < TestScoreList.Length; i++)
+		{
+			Write("Enter a test score: ");
+			int score = Convert.ToInt32(ReadLine());
+			TestScoreList[i] = score;
+			total += score;
+		}
+
+		int average = total / TestScoreList.Length;
+		
+		for (int i = 0; i < TestScoreList.Length; i++)
+		{
+			int difference = TestScoreList[i] - average;
+			Write("Test # " + i + ":    " + TestScoreList[i] + " From average:    " +
+			+ difference + "\n");
+		}
 	}
 }
