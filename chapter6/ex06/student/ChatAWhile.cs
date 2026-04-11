e7f4d7cb6cea34fa61436388675dd37080b0c320
evapeck <evalpeck@gmail.com>
2026-04-11 20:41:37 +0000

    Testing Sat Apr 11 2026 20:41:37 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex06/student/ChatAWhile.cs
+++ chapter6/ex06/student/ChatAWhile.cs
@@ -5,6 +5,22 @@ class ChatAWhile
 {
 	static void Main()
 	{
-		// Write your code here
+		int[] areaCodes = {262, 414, 608, 715, 815, 920};
+		double[] rates = {0.07, 0.10, 0.05, 0.16, 0.24, 0.14};
+
+		WriteLine("Enter an area code: ");
+		int inputArea = Convert.ToInt32(ReadLine());
+		WriteLine("Enter length of call in minutes: ");
+		int callLength = Convert.ToInt32(ReadLine());
+
+		int index = Array.IndexOf(areaCodes, inputArea);
+
+		if (index != -1)
+		{
+			double totalCost = callLength * rates[index];
+			WriteLine("Your phone call to area {0} costs {1} per minute \nFor {2} minutes the total is {3}.", inputArea, rates[index].ToString("C", CultureInfo.GetCultureInfo("en-US")), callLength, totalCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+		}
+		else 
+			WriteLine("Sorry - no calls allowed to area " + inputArea);
 	}
 }
